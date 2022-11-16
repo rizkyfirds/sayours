3d0b8d0db23709edebd6703fadaa500b76ff48ff
Muhammad Daffa Ferdiansyah <daffaferdiansyah48@gmail.com>
2022-11-17 05:37:08 +0700

    'a'

--- src/pages/EditProfile.js
+++ src/pages/EditProfile.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import logo from '../aset/sayoursputih.png'
 
 function EditProfile() {
   return (
-    <div>EditProfile</div>
+    <div>
+      <div className='flex h-screen'>
+        <div className='h-1/4 w-full bg-purple-900/60'>
+          <div className='logo h-36 w-44'>
+          <img src={logo} alt="" />
+        </div>
+        <div className='h-screen w-full px-96 bg-primary'>
+          <div className=' fixed top-280 left-20 bottom-14 px-36 py-56 bg-violet-100/50 rounded-lg'>
+            <p className='fixed top-64 m-auto text-xl font-bold font-Montserrat'>Lele Terbang</p>
+            <p className='fixed top-96 left-40 m-auto text-lg font-bold font-Montserrat'>Saldo: Rp67.999</p>
+          </div>
+          <div className=' fixed top-280 right-20 bottom-14 w-2/3 py-56 bg-violet-100/50 rounded-lg'>
+
+          </div>
+        </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default EditProfile
+export default EditProfile
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -2,19 +2,28 @@ import React from 'react'
 import logo from '../aset/sayoursputih.png'
 function Homepage() {
   return (
-    <div className='grid grid-cols-5 grid-flow-row-dense'>
-      <div className='bg-red-500 min-h-[50px]'>
+    <div className='grid grid-cols-10 grid-flow-row-dense'>
+      <div className='bg-purple-900/60 min-h-[50px] col-span-2'>
         <div className='logo'>
           <img src={logo} alt="" />
         </div>
       </div>
-      <div className='bg-blue-100 col-span-3'>
-        <div>
-          <input type="text" className='outline outline-offset-2 outline-1 w-80 h-75 text-left bg-white-500 text-black' placeholder='Lagi cari apa nih...' />
+      <div className='bg-purple-900/60 min-h-[25px] col-span-1'>
+
+      </div>
+      <div className='bg-purple-900/60 min-h-[25px] col-span-1'>
+      <div>
+          <input type="text" className='outline outline-offset-2 outline-1 w-100 h-10 fixed top-10 left-50 px-20 py-5 text-left bg-white-500 text-black' placeholder='Lagi cari apa nih...' />
         </div>
       </div>
-      <div className='bg-red-100 min-h-[25px] col-span-1'>
+      <div className='bg-purple-900/60 col-span-4'>
+        
+      </div>
+      <div className='bg-purple-900/60 min-h-[25px] col-span-2'>
 
+      </div>
+      <div className='bg-violet-500 min-h-[50px] w-full'>
+        <p className='fixed top-28 left-17 m-auto text-xl font-bold font-Montserrat'>Sayuran Segar</p>
       </div>
     </div>
   );
