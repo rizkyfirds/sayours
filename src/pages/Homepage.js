f76e5fa176a69609ec99fd85cff4ae7fd7427687
Muhammad Daffa Ferdiansyah <daffaferdiansyah48@gmail.com>
2022-11-16 23:53:14 +0700

    nyerah

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -2,13 +2,20 @@ import React from 'react'
 import logo from '../aset/sayoursputih.png'
 function Homepage() {
   return (
-    <div className='grid grid-cols-5 gap-x-2 gap-y-2 grid-flow-row-dense'>
+    <div className='grid grid-cols-5 grid-flow-row-dense'>
       <div className='bg-red-500 min-h-[50px]'>
         <div className='logo'>
           <img src={logo} alt="" />
         </div>
       </div>
-      
+      <div className='bg-blue-100 col-span-3'>
+        <div>
+          <input type="text" className='outline outline-offset-2 outline-1 w-80 h-75 text-left bg-white-500 text-black' placeholder='Lagi cari apa nih...' />
+        </div>
+      </div>
+      <div className='bg-red-100 min-h-[25px] col-span-1'>
+
+      </div>
     </div>
   );
 }
