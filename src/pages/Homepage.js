6581b4f23eb67c0508363b8cea5ef271a17c93fd
Muhammad Daffa Ferdiansyah <daffaferdiansyah48@gmail.com>
2022-11-16 20:58:58 +0700

    a

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 function Homepage() {
   return (
-    <div>Home</div>
+    <div >
+      <div className='flex h-screen'>
+        <div className='m-auto px-24 py-40 bg-violet-100/10 rounded-lg'>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
