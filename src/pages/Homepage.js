dabe68b00ca9bc8d1c99a964632f8ef2d7011faa
Muhammad Daffa Ferdiansyah <daffaferdiansyah48@gmail.com>
2022-11-16 22:41:13 +0700

    nambah logo

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,15 +1,16 @@
 import React from 'react'
-
+import logo from '../aset/sayoursputih.png'
 function Homepage() {
   return (
-    <div >
-      <div className='flex h-screen'>
-        <div className='m-auto px-24 py-40 bg-violet-100/10 rounded-lg'>
-
+    <div className='grid grid-cols-5 gap-x-2 gap-y-2 grid-flow-row-dense'>
+      <div className='bg-red-500 min-h-[50px]'>
+        <div className='logo'>
+          <img src={logo} alt="" />
         </div>
       </div>
+      
     </div>
-  )
+  );
 }
 
 export default Homepage
