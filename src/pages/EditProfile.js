d0c465d42cb9f263d92765899972793d26f46eed
Muhammad Rizky Firdaus <rizkyfirdaus567@gmail.com>
2022-11-17 14:35:22 +0700

    hai pentol

--- src/pages/EditProfile.js
+++ src/pages/EditProfile.js
@@ -1,31 +1,44 @@
 import React from 'react'
-import logo from '../aset/SaYours-1 (1).png'
+import logo from '../aset/sayoursputih.png'
 import duit from '../aset/saldo.png'
 
-function EditProfile() {
+export default function EditProfile() {
   return (
     <div>
-      <div className='flex h-screen'>
-        <div className='h-1/4 w-full bg-purple-900/60'>
-          <div className='logo h-36 w-44'>
-          <img src={logo} alt="" />
-        </div>
-        <div className='h-screen w-full px-96 bg-primary'>
-          <div className=' fixed top-280 left-20 bottom-14 px-36 py-56 bg-violet-100/50 rounded-lg'>
-            <p className='fixed top-64 m-auto text-xl font-bold font-Montserrat'>Lele Terbang</p>
-            <div className='duit h-36 w-44'>
-            <img src={duit} alt="" />
-            </div>
-            <p className='fixed top-96 left-40 m-auto text-lg font-bold font-Montserrat'>Saldo: Rp67.999</p>
-          </div>
-          <div className='  py-56 bg-violet-100/50 rounded-lg'>
-
-          </div>
-        </div>
+      <div className='flex bg-purple-900 py-2'>
+        <div>
+          <img src={logo} className='w-72' alt="" />
         </div>
       </div>
+      <div className='flex bg-primary h-screen'>
+        <div className="grid grid-cols-3 gap-5 mt-5 px-10">
+          <div className='w-[28rem] h-[34rem] bg-gray-300 rounded-xl bg-cover bg-center relative'>
+            <div className='flex items-center m-10'>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-24 h-24">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+              </svg>
+              <div className=' text-4xl font-Montserrat m-6 font-bold'>
+                Lele Terbang
+              </div>
+            </div>
+              <div className='grid grid-cols-3 gap-5 mt-4 px-10'>
+                <div>
+                  <div className='cols-3'>
+                    <img src={duit} className='w-12' alt="" />
+                  </div>
+                  <div className=' w-2/3 text-2xl font-Montserrat m-6 font-bold cols-end'>
+                    saldo
+                  </div>
+                </div>
+
+            </div>
+          </div>
+          <div className='w-[60rem] h-[34rem] bg-gray-300 rounded-xl bg-cover bg-center relative'>
+          </div>
+        </div>
+
+      </div>
     </div>
+    
   )
 }
-
-export default EditProfile
