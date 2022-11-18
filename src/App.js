6ca184593d1e8fe8a2911187e70ffbb880f13163
Muhammad Daffa Ferdiansyah <daffaferdiansyah48@gmail.com>
2022-11-18 10:40:46 +0700

    k

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import EditProfile from './pages/EditProfile';
 import Homepage from './pages/Homepage';
+import Transaction_1 from './pages/Transaction-1';
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
         <Route path="/register" element={<Register  />} />
         <Route path="/editprofile" element={<EditProfile  />} />
         <Route path="/homepage" element={<Homepage  />} />
+        <Route path="/transaction-1" element={<Transaction_1 />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Transaction-1.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Transaction_1() {
+  return (
+    <div>
+        <div>
+            <div className='flex bg-purple-900 py-20'>
+            </div>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Transaction_1
